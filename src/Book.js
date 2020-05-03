6c434c42728423b30ae3a3629e6d79e955681d2c
Himel Das <himeldas66@gmail.com>
2020-05-03 22:21:46 +0600

    refactor

--- src/Book.js
+++ src/Book.js
@@ -8,12 +8,24 @@ const Book = props => {
         props.onUpdateShelf(book, event.target.value);
     };
 
+    // Handle the search to work correctly when a book does not have a thumbnail
+    const getThumbnail = (book) => {
+        try {
+            // Try to send the thumbnail URL
+            return book.imageLinks.thumbnail;
+        }
+        catch(err) {
+            // If try failes end empty string
+            return '';
+        }
+    };
+
     // console.log(book);
 
     return (
         <div className="book">
             <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${getThumbnail(book)})` }}></div>
                 <div className="book-shelf-changer">
                     <select onChange={handleOnUpdateShelf} defaultValue={book.shelf}>
                         <option value="move" disabled>Move to...</option>
